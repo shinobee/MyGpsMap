091844c84f646c9a4465c59972a4535219350f2e
shinobee <shinobee@zen-zee.com>
2019-11-24 09:09:15 +0900

    Initial Commit

---
+++ MyGpsMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGpsMap
+//
+//  Created by shinobee on 2019/11/24.
+//  Copyright © 2019 shinobee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
